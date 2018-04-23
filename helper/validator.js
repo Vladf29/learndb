05dc19e8e25f8b8e391e5307ed3bb21c3aee1e4d
Vladf29 <boxforemailand@gmail.com>
2018-04-23 15:05:20 +0300

    validator

--- helper/validator.js
+++ helper/validator.js
@@ -1,1 +1,18 @@
 'use strict'
+
+const Joi = require('joi');
+
+module.exports = {
+    validator: function (schema) {
+        return (req, res, next) => {
+            if (!req.body) return res.status(400).send();
+            const result = Joi.validate(req.body, schema);
+            if (result.error) return res.status(400).send(result.error.message);
+            next();
+        }
+    },
+    schemas: {
+        name: Joi.string().min(2).required(),
+        age: Joi.string().min(2).required()
+    }
+}
--- router/users.js
+++ router/users.js
@@ -4,13 +4,17 @@ const express = require('express');
 const router = express.Router();
 
 const dbUsers = require('../db/users');
+const {
+    validator,
+    schemas
+} = require('../helper/validator');
 
 router.route('/users')
     .get(dbUsers.get)
-    .post(dbUsers.post, (req, res) => {
+    .post(validator(schemas), dbUsers.post, (req, res) => {
         res.send('OK');
     })
-    .put(dbUsers.put, (req, res) => {
+    .put(validator(schemas), dbUsers.put, (req, res) => {
         res.send('OK');
     })
     .delete(dbUsers.delete, (req, res) => {
